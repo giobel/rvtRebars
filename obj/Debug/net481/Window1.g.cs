9ed5b7161672f3f6be273043e9984a5d8ce84f69
revitApiJourney <revitApiJourney@gmail.com>
2025-05-13 13:59:35 +0800

    update

--- obj/Debug/net481/Window1.g.cs
+++ obj/Debug/net481/Window1.g.cs
@@ -68,7 +68,7 @@ namespace rvtRebars {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/rvtRebars;component/window1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/rvtRebars;V1.0.0.0;component/window1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net481/rvtRebars.AssemblyInfo.cs
+++ obj/Debug/net481/rvtRebars.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rvtRebars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3922cc283a9ff14514924de66136a2ea3390c87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520a0c34fc8bddf25b99ac953128b191d456fcea")]
 [assembly: System.Reflection.AssemblyProductAttribute("rvtRebars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rvtRebars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
